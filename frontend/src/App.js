90719d36246241834588ca09905246d3d78a7188
Abdalla <a.eltayeb@campus.tu-berlin.de>
2023-12-04 18:27:51 -0800

    user input working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,53 @@ import React, { useState, useEffect } from 'react'
 function App() {
 
   const [data, setData] = useState([{}])
+  
 
-  useEffect(() => {
-    fetch("/ScrapeThenAnalyze").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  })
+  function analyze() {
+    let twitterhandle = document.getElementById("twitterhandle").value
+    if(twitterhandle !== "") {
+      fetch("/ScrapeThenAnalyze/" + twitterhandle).then(
+        res => res.json()
+      ).then(
+        data => {
+          setData(data)
+          console.log(data)
+        }
+      )
+    }  
+  }
+    
+  
 
 
   return(
     <div>
+      <div>
+        Twitter handle:
+        <input type = "text" id ="twitterhandle"/>
+      </div>
+      
+      <button onClick = {()=>analyze()}>Submit</button>
       {(typeof data.analysis === 'undefined') ? (
         <p>Loading...</p>
       ): (
         <p>{data.analysis}</p>
       )}
+      
     </div>
   )
 }
 
 export default App
 
+/*
+  <button onClick = {() => {{
+        (typeof data.analysis === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        <p>{data.analysis}</p>
+      )}}}>Click me</button>
+*/
 /*
  {(typeof data.members === 'undefined') ? (
         <p>Loading...</p>
